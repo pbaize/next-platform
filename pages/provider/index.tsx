ccfe6229b4b7850ff56008a68bb8ed38803d8d31
Pierre Baize <pierre@openfin.co>
2020-09-25 13:34:40 -0600

    pwa

--- pages/provider/index.tsx
+++ pages/provider/index.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
+import Link from 'next/link';
+
 
 export default function app() {
     // Pass in empty array to only run once
     useEffect(() => {
         fin.Platform.init();
     }, []);
-    return <div>Custom Provider</div>;
+    return <div>
+        <Link href="platform-window" >Custom Window (for prefetch)</Link>
+    </div>;
 };
